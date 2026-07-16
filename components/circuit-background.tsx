d7027c1749ad75b05e0f6601a6df483141de22e7
Aminata Ndiaye <proaminatandiaye@gmail.com>
2026-07-16 22:35:08 +0000

    Update circuit-background.tsx

--- components/circuit-background.tsx
+++ components/circuit-background.tsx
@@ -1,15 +1,15 @@
 export function CircuitBackground() {
   return (
-    <div className="pointer-events-none fixed inset-0 -z-10 h-full w-full bg-[#000000]">
+    <div className="fixed inset-0 h-full w-full bg-black" style={{ zIndex: 0 }}>
+      {/* Grille avec une couleur très contrastée */}
       <div 
-        className="absolute inset-0 opacity-[0.2]" 
+        className="absolute inset-0 opacity-[0.5]" 
         style={{
-          backgroundImage: `linear-gradient(to right, #d1310a 1px, transparent 1px), 
-                            linear-gradient(to bottom, #d1310a 1px, transparent 1px)`,
-          backgroundSize: '60px 60px'
+          backgroundImage: `linear-gradient(to right, white 2px, transparent 2px), 
+                            linear-gradient(to bottom, white 2px, transparent 2px)`,
+          backgroundSize: '100px 100px'
         }}
       />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-[#d1310a]/10 blur-[120px] rounded-full" />
     </div>
   )
 }
